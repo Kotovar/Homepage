5cc7097d1c9fd7884b4bfca5a22de564bb26180d
Kotovar <neasit3@gmail.com>
2023-12-11 22:46:48 +0700

    create main files and do Webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Output Management',
+			template: './src/main.html',
+		}),
+	],
+	devtool: 'inline-source-map',
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.html$/i,
+				loader: 'html-loader',
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+		],
+	},
+};
