595279c706f41f592d45c2ee48dcf3974f9ebda7
Kotovar <neasit3@gmail.com>
2024-01-05 23:32:28 +0700

    add some changes with main page, add photo, add js for year in footer, add some styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import '../dist/css/style.css';
+
+const date = new Date();
+const currentYear = date.getFullYear();
+const year = document.getElementById('year');
+year.textContent = `© ${currentYear} | Designed and coded by Dmitry`;
