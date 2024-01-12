f56d2e6bde3a28a9965fbd5b219e28783024afed
Kotovar <neasit3@gmail.com>
2024-01-13 00:33:29 +0700

    add :not(.current):hover selector, fix colors, change avatar border, add styles for slidebar using css and js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,15 @@ const date = new Date();
 const currentYear = date.getFullYear();
 const year = document.getElementById('year');
 year.textContent = `© ${currentYear} | Designed and coded by Dmitry`;
+
+// проверка на наличие скроллбара
+const html = document.querySelector('html');
+const footer = document.querySelector('footer');
+const panel = document.querySelector('.header__panel');
+const rightIndent =
+	getComputedStyle(html).getPropertyValue('--scrollbar-width');
+const hasScrollbar = html.clientHeight < html.scrollHeight;
+if (!hasScrollbar) {
+	panel.style.marginRight = rightIndent;
+	footer.style.paddingRight = rightIndent;
+}
